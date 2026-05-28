7c0220a5ca884da4d287401bac40d6a7f3350cf7
Moises203548 <mortegas4@unicartagena.edu.co>
2026-05-27 22:17:31 -0500

    feat(domain): crea enum TipoDestino y actualiza Destino

---
+++ src/main/java/co/edu/udec/desechosfabrica/domain/enums/TipoDestino.java
@@ -0,0 +1,7 @@
+package co.edu.udec.desechosfabrica.domain.enums;
+
+public enum TipoDestino {
+    RELLENO_SANITARIO,
+    PLANTA_TRATAMIENTO,
+    RECICLAJE
+}
--- src/main/java/co/edu/udec/desechosfabrica/domain/model/Destino.java
+++ src/main/java/co/edu/udec/desechosfabrica/domain/model/Destino.java
@@ -1,18 +1,21 @@
 package co.edu.udec.desechosfabrica.domain.model;
 
+import co.edu.udec.desechosfabrica.domain.enums.TipoDestino;
+import co.edu.udec.desechosfabrica.domain.enums.TipoResiduo;
+
 public class Destino {
 
     private final int idDestino;
     private String nombre;
-    private String tipoDestino;
+    private TipoDestino tipoDestino;
     private String ciudad;
 
-    public Destino(int idDestino, String nombre, String tipoDestino, String ciudad) {
+    public Destino(int idDestino, String nombre, TipoDestino tipoDestino, String ciudad) {
 
         if (nombre == null || nombre.isBlank())
             throw new IllegalArgumentException("El nombre del destino no puede estar vacio");
-        if (tipoDestino == null || tipoDestino.isBlank())
-            throw new IllegalArgumentException("El tipo de destino no puede estar vacio");
+        if (tipoDestino == null)
+            throw new IllegalArgumentException("El tipo de destino no puede ser nulo");
         if (ciudad == null || ciudad.isBlank())
             throw new IllegalArgumentException("La ciudad no puede estar vacia");
 
@@ -27,7 +30,7 @@ public class Destino {
     public String getNombre() {
         return nombre;
     }
-    public String getTipoDestino() {
+    public TipoDestino getTipoDestino() {
         return tipoDestino;
     }
     public String getCiudad() {
